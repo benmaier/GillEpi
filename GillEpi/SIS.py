a11ec9f7efb3a036701bd26406cae13a39a0e48f
Benjamin Maier <benjaminfrankmaier@gmail.com>
2016-04-28 11:38:04 +0200

    changed SIS to be inherit of SIR

--- GillEpi/SIS.py
+++ GillEpi/SIS.py
@@ -4,9 +4,10 @@ import random
 import numpy as np
 import networkx as nx
 
-from SIR_node import SIR_node
+from GillEpi.SIR_node import SIR_node
+from GillEpi.SIR import SIR
 
-class SIS():
+class SIS(SIR):
 
     def __init__(self,
                  G,              # (dynamic) network. beware! this is going to change during simulation
@@ -21,110 +22,18 @@ class SIS():
                  save_everything = False
                 ):
 
-
-        if nx.is_directed(G):
-            raise TypeError("G is directed but needs to be undirected")
-
-        self.G = G
-
-        self.rewire_function = rewire_function
-        self.mean_degree = mean_degree_function
-
-        self.verbose = verbose
-
-        if self.rewire_function is None:
-            self.rewiring_rate = 0.
-        else:
-            self.rewiring_rate = rewiring_rate
-
-        self.infection_rate = infection_rate
-        self.recovery_rate = recovery_rate
-
-        self.rates = np.array( [ infection_rate, recovery_rate, rewiring_rate ] )
-
-        self.infected = set()
-        self.recovered = set()
-        self.nodes = set(G.nodes())
-
-        self.SIR_nodes = { n:SIR_node() for n in self.nodes }
-
-        # add vaccinated nodes (are in recovered class right from the beginning)
-        if vaccinated>0:
-            if hasattr(infection_seeds,"__len__"):
-                to_choose_from = self.nodes() - set(infection_seeds)
-            else:
-                to_choose_from = self.nodes()
-
-            vaccinated_nodes = random.sample(to_choose_from,vaccinated)
-            self.recovered.update(vaccinated_nodes)
-            
-            for n in vaccinated_nodes:
-               self.SIR_nodes[n].set_recovered()
-
-
-        # add infected seed nodes
-        if hasattr(infection_seeds,"__len__"):
-            seed_nodes = infection_seeds
-            infection_seeds = len(infection_seeds)
-        else:
-            seed_nodes = random.sample(self.get_susceptibles(),infection_seeds)
-
-        self.infected.update(seed_nodes)
-        for n in seed_nodes:
-            self.SIR_nodes[n].set_infected()
-
-        # process new edges for SI links
-        self.SI_links = set()
-        for newly_inf in self.infected:
-            new_edges = [ (newly_inf, n) for n in G.neighbors(newly_inf) ]
-            removed_SI_links, new_SI_links = self.get_removed_and_new_SI_links_from_edge_list(new_edges)
-            self.SI_links.update(new_SI_links)
-            self.SI_links.difference_update(removed_SI_links)
-
-        self.t = 0.
-        self.s_of_t = [ [ 0., self.s() ] ]
-        self.i_of_t = [ [ 0., self.i() ] ]
-        self.r_of_t = [ [ 0., self.r() ] ]
-
-        if self.mean_degree is not None:
-            self.k_of_t = [ [ 0., self.mean_degree(G) ] ]
-
-
-    def get_event_rates(self):
-        return np.array([ 
-                          self.number_of_SI_links() * self.infection_rate,
-                          self.number_of_infected() * self.recovery_rate,
-                          self.G.number_of_nodes()  * self.rewiring_rate
-                        ],dtype=float)
-
-    """
-    def is_susceptible(self,node):
-        return (node not in self.infected) and (node not in self.recovered)
-
-    def get_removed_and_new_SI_links_from_edge_list(self,edgelist):
-        new_SI = []
-        removed_SI = []
-        [ 
-              new_SI.append(e) \
-              if ( (e[0] in self.infected and self.is_susceptible(e[1]) ) or \
-                   (e[1] in self.infected and self.is_susceptible(e[0]) ) )  \
-              else removed_SI.extend([e,(e[1],e[0])]) \
-              for e in edgelist
-        ]
-        return removed_SI, new_SI
-    """
-
-    def get_removed_and_new_SI_links_from_edge_list(self,edgelist):
-        new_SI = []
-        removed_SI = []
-        [ 
-              new_SI.append(e) \
-              if ( (self.SIR_nodes[e[0]].is_infected() and self.SIR_nodes[e[1]].is_susceptible() ) or \
-                   (self.SIR_nodes[e[1]].is_infected() and self.SIR_nodes[e[0]].is_susceptible() ) ) \
-              else removed_SI.extend([e,(e[1],e[0])]) \
-              for e in edgelist
-        ]
-        return removed_SI, new_SI
+        SIR.__init__(self,
+                 G,              # (dynamic) network. beware! this is going to change during simulation
+                 infection_rate, # per S-I link
+                 recovery_rate,  # per individual
+                 rewiring_rate = rewiring_rate,  # per individual
+                 infection_seeds = infection_seeds,
+                 vaccinated = vaccinated,
+                 rewire_function = rewire_function,
+                 mean_degree_function = mean_degree_function,
+                 verbose = verbose,
+                 save_everything = save_everything
+                )
 
     def recover_event(self):
 
@@ -141,82 +50,6 @@ class SIS():
             print("deleted",deleted_edges)
 
         self.SI_links.difference_update(deleted_edges)
-
-    def infection_event(self):
-        if self.verbose:
-            print("============= infection event")
-            print("infected:", self.infected)
-            print("recovered:", self.recovered)
-            print("SI link", self.SI_links)
-
-        infective_link = random.sample(self.SI_links,1)[0]
-
-        if self.verbose:
-            print("infective_link:", infective_link)
-
-        if self.SIR_nodes[ infective_link[0] ].is_susceptible() and self.SIR_nodes[ infective_link[1] ].is_infected():
-            newly_inf = infective_link[0]
-            self.infected.add(infective_link[0])            
-        elif self.SIR_nodes[ infective_link[1] ].is_susceptible() and self.SIR_nodes[ infective_link[0] ].is_infected():
-            newly_inf = infective_link[1]
-            self.infected.add(infective_link[1])
-        else:
-            print("edge:", infective_link)
-            print(infective_link[0],"S  :",self.SIR_nodes[ infective_link[0] ].is_susceptible())
-            print(infective_link[0],"I  :",self.SIR_nodes[ infective_link[0] ].is_infected())
-            print(infective_link[1],"S  :",self.SIR_nodes[ infective_link[1] ].is_susceptible())
-            print(infective_link[1],"I  :",self.SIR_nodes[ infective_link[1] ].is_infected())
-            raise ValueError("There was a non SI-link in the array of SI links. This shouldn't happen.")        
-
-        self.SIR_nodes[newly_inf].set_infected()
-
-        # process new edges for SI links
-        new_edges = [ (newly_inf, n) for n in self.G.neighbors(newly_inf) ]
-        removed_SI_links,new_SI_links = self.get_removed_and_new_SI_links_from_edge_list(new_edges)
-
-        if self.verbose:
-            print("now infected:", self.infected)
-            print("potential new_SI_links:", new_edges)
-            print("new_SI_links:", new_SI_links)
-            print("removed_SI_links:", removed_SI_links)
-
-        self.SI_links.update(new_SI_links)
-        self.SI_links.difference_update(removed_SI_links)
-
-
-    def rewire_event(self):
-
-        # get deleted and new edges from rewiring 
-        deleted_edges, new_edges = self.rewire_function()
-
-        if self.verbose:
-            print("============= rewiring event")
-            print("rewired:", deleted_edges,new_edges)
-
-        adjoint_deleted_edges = [ (e[1],e[0]) for e in deleted_edges ]
-
-        # remove all edges from SI links that have been removed
-        # due to rewiring
-        self.SI_links.difference_update(deleted_edges)
-        self.SI_links.difference_update(adjoint_deleted_edges)
-
-        # process new edges for SI links
-        removed_SI_links, new_SI_links = self.get_removed_and_new_SI_links_from_edge_list(new_edges)
-        self.SI_links.update(new_SI_links)
-        self.SI_links.difference_update(removed_SI_links)
-
-    def choose_tau_and_event(self):
-        rates = self.get_event_rates()
-        total_rate = rates.sum()
-        tau = np.random.exponential(1./total_rate)
-        try:
-            event = np.random.choice(len(rates),p=rates/total_rate)
-        except ValueError as e:
-            print("rates:", rates)
-            print("total_rate:", total_rate)
-            raise ValueError(e)
-
-        return tau, event
 
     def event(self):
 
@@ -241,45 +74,6 @@ class SIS():
 
         while self.t < tmax:
             self.event()
-
-    def number_of_SI_links(self):
-        return len(self.SI_links)
-
-    def get_susceptibles(self):
-        return (self.nodes - self.infected) - self.recovered
-
-    def number_of_susceptibles(self):
-        return self.G.number_of_nodes() - self.number_of_infected() - self.number_of_recovered()
-
-    def number_of_infected(self):
-        return len(self.infected)
-
-    def number_of_recovered(self):
-        return len(self.recovered)
-
-    def S(self):
-        return self.number_of_susceptibles()
-
-    def I(self):
-        return self.number_of_infected()
-
-    def R(self):
-        return self.number_of_recovered()
-
-    def s(self):
-        return self.S() / float(self.G.number_of_nodes())
-
-    def i(self):
-        return self.I() / float(self.G.number_of_nodes())
-
-    def r(self):
-        return self.R() / float(self.G.number_of_nodes())
-
-    def get_t_R0(self):        
-        if self.mean_degree is not None:
-            k_of_t = np.array(self.k_of_t)
-            t, k = k_of_t[:,0], k_of_t[:,1]
-            return t, k * self.infection_rate / self.recovery_rate
 
 
             
