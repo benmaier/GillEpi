203d1fec10128f78e1cc93a39c1ef88499d3d2de
Benjamin Maier <benjaminfrankmaier@gmail.com>
2016-04-28 14:29:38 +0200

    added SEIR ndoe

--- GillEpi/node_classes.py
+++ GillEpi/node_classes.py
@@ -26,3 +26,39 @@ class SIR_node():
         self.recovered = False
 
 
+class SEIR_node():
+
+    def __init__(self):
+        self.set_susceptible()
+
+    def is_infected(self):
+        return self.infected
+
+    def is_recovered(self):
+        return self.recovered
+
+    def is_exposed(self):
+        return self.exposed
+
+    def is_susceptible(self):
+        return not self.infected and not self.recovered and not self.exposed
+
+    def set_infected(self):
+        self.infected = True
+        self.recovered = False
+        self.exposed = False
+
+    def set_exposed(self):
+        self.infected = False
+        self.recovered = False
+        self.exposed = True
+
+    def set_recovered(self):
+        self.infected = False
+        self.recovered = True
+        self.exposed = False
+
+    def set_susceptible(self):
+        self.infected = False
+        self.recovered = False
+        self.exposed = False
