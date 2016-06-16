edd6e84e14ae317dbe401a19bbae597ec16a6009
Benjamin Maier <benjaminfrankmaier@gmail.com>
2016-06-16 16:05:35 +0200

    version up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='GillEpi',
-      version='0.0.3',
+      version='0.0.4',
       description='Provides classes to simulate epidemics on (potentially time varying) networks using a Gillespie stochastic simulation algorithm.',
       url='https://www.github.com/benmaier/GillEpi',
       author='Benjamin F. Maier',
